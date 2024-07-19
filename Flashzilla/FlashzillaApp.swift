b812113ef88308bf575d05d9fbee215d18ee18ec
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-19 18:16:57 -0400

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Parth Antala on 2024-07-19.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
