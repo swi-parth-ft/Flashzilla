1aab69599720477e57a95502a38cc8f9374611c9
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-20 13:47:26 -0400

    Move cards with DragGesture and offset

--- Flashzilla/CardView.swift
+++ Flashzilla/CardView.swift
@@ -11,6 +11,8 @@ struct CardView: View {
     
     let card: Card
     @State private var isShowingAnswer = false
+    @State private var offset = CGSize.zero
+    var removal: (() -> Void)? = nil
     
     var body: some View {
         ZStack {
@@ -32,6 +34,22 @@ struct CardView: View {
             .multilineTextAlignment(.center)
         }
         .frame(width: 450, height: 250)
+        .rotationEffect(.degrees(offset.width / 5.0))
+        .offset(x: offset.width * 5)
+        .opacity(2 - Double(abs(offset.width / 50)))
+        .gesture (
+            DragGesture()
+                .onChanged { value in
+                    offset = value.translation
+                }
+                .onEnded { _ in
+                    if abs(offset.width) > 100 {
+                        removal?()
+                    } else {
+                        offset = .zero
+                    }
+                }
+        )
         .onTapGesture {
             isShowingAnswer.toggle()
         }
