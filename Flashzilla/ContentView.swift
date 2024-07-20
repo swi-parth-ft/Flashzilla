dd9c5987540d96efc45bf4ad4e81792876279c87
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-20 12:32:48 -0400

    Add basic gestures

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -8,12 +8,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var currentAmount = 0.0
+    @State private var finalAmount = 1.0
+    @State private var currentAngle = Angle.zero
+    @State private var finalAngle = Angle.zero
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Long press")
+                .onLongPressGesture(minimumDuration: 2) {
+                    print("Long pressed")
+                } onPressingChanged: { inProgress in
+                    print("In progress: \(inProgress)")
+                }
+            
+            Text("Zoom")
+                        .scaleEffect(finalAmount + currentAmount)
+                        .gesture(
+                            MagnifyGesture()
+                                .onChanged { value in
+                                    currentAmount = value.magnification - 1
+                                }
+                                .onEnded { value in
+                                    finalAmount += currentAmount
+                                    currentAmount = 0
+                                }
+                        )
+            
+            
+            Text("rotation")
+                .rotationEffect(finalAngle + currentAngle)
+                .gesture(
+                    RotateGesture()
+                        .onChanged { value in
+                            currentAngle = value.rotation
+                        }
+                        .onEnded { value in
+                            finalAngle += currentAngle
+                            currentAngle = .zero
+                        }
+                )
         }
         .padding()
     }
