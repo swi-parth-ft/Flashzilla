bd753d42d66e7022ef6aa0a75db8b5c4f3e9cff9
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-20 12:57:53 -0400

    Scene Phases

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -9,20 +9,20 @@ import SwiftUI
 
 struct ContentView: View {
     
-    let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
-    @State private var counter = 0
+    @Environment(\.scenePhase) var scenePhase
     
     var body: some View {
         Text("Start Timer")
-            .onReceive(timer) { time in
-                if counter == 5 {
-                    timer.upstream.connect().cancel()
-                } else {
-                    print("the time is \(time)")
+            .onChange(of: scenePhase) { oldValue, newValue in
+                if newValue == .active {
+                    print("Active")
+                } else if newValue == .inactive {
+                    print("Inactive")
+                } else if newValue == .background
+                {
+                    print("Background")
                 }
-                
-                counter += 1
-            }
+                }
     }
 }
 
