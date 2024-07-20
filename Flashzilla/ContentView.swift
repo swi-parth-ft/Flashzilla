2307efd271d91688089212455cf92063dde77e0f
Parth Antala <52197140+parallel-appy@users.noreply.github.com>
2024-07-20 13:28:31 -0400

    Build stack of cards

--- Flashzilla/ContentView.swift
+++ Flashzilla/ContentView.swift
@@ -7,12 +7,31 @@
 
 import SwiftUI
 
+
+extension View {
+    func stacked(at position: Int, in total: Int) -> some View {
+        let offset = Double(total - position)
+        return self.offset(y: offset * 10)
+    }
+}
 struct ContentView: View {
     
-    @Environment(\.scenePhase) var scenePhase
+    @State private var cards = Array<Card>(repeating: .example, count: 10)
     
     var body: some View {
-        CardView(card: .example)
+        ZStack {
+            VStack {
+                ZStack {
+                    Image("background")
+                        .resizable()
+                        .ignoresSafeArea()
+                    ForEach(0..<cards.count, id: \.self) { index in
+                        CardView(card: cards[index])
+                            .stacked(at: index, in: cards.count)
+                    }
+                }
+            }
+        }
     }
 }
 
